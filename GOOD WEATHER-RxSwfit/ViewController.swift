7ab349d0c42b27eacc34174944b370ef4222d703
Yuta iwasaki <yanyong0127@gmail.com>
2022-01-13 18:17:02 +0900

    commitできないのなんで

---
+++ GOOD WEATHER-RxSwfit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GOOD WEATHER-RxSwfit
+//
+//  Created by IwasakIYuta on 2022/01/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
